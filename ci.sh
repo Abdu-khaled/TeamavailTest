065e8a70257a7252f2f1b467dfc3feceef8522ab
Abdu-khaled <abdelrahmankhalid27@gmail.com>
2025-09-18 14:38:16 +0300

    feat(ci): add ci.sh script for lint, test, docker build, and run

---
+++ ci.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+APP_NAME="availability-tracker"
+TAG="latest"
+
+echo "Running lint checks..."
+if ! npx eslint .; then
+  echo "ESLint issues found!"
+fi
+
+if ! npx prettier --check .; then
+  echo "Prettier formatting issues found!"
+fi
+
+echo "Running tests..."
+if npm run | grep -q "test"; then 
+  npm install
+  npm test
+else
+  echo "No test script found!!"
+fi 
+
+
+echo " Building Docker image..."
+docker build --cache-from=$APP_NAME:$TAG -t $APP_NAME:$TAG .
+
+
+echo "Starting application with Docker Compose..."
+
+docker compose down || true
+docker compose up -d --build 
+
+echo "CI pipeline completed successfully!"
